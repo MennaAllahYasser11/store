c7799110ad646e59926bad0aa4ac249f5ca4298f
Menna Allah Yasser <mennaallahyasser111@gmail.com>
2025-06-02 19:38:01 +0300

    feat: add generator token to reuse it

---
+++ ebar-store/src/Utils/token.js
@@ -0,0 +1,25 @@
+export const generateToken = () => {
+  const chars = "0123456789abcdef";
+  let token = "";
+  for (let i = 0; i < 64; i++) {
+    token += chars[Math.floor(Math.random() * chars.length)];
+  }
+  return token;
+};
+
+export const getSessionToken = () => {
+  let token = localStorage.getItem("sessionToken");
+  if (!token) {
+    token = generateToken();
+    localStorage.setItem("sessionToken", token);
+  }
+  return token;
+};
+
+export const checkAuth = () => {
+  return localStorage.getItem("authToken");
+};
+
+export const getAuthToken = () => {
+  return localStorage.getItem("authToken");
+};
