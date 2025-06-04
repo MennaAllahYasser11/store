331a71c833f9d0e80853814f50072e04b96a0f41
Menna Allah Yasser <mennaallahyasser111@gmail.com>
2025-06-04 22:30:03 +0300

    improve code to generate sessionToken if not generated once

--- ebar-store/src/api/axiosConfig.js
+++ ebar-store/src/api/axiosConfig.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-
+import { getSessionToken } from "../Utils/token";
 const axiosInstance = axios.create({
   baseURL: "https://dev.backend-api.goldady.com",
   headers: {
@@ -11,12 +11,20 @@ axiosInstance.interceptors.request.use(
   (config) => {
     console.log("request config is ", config); // to ensure config is sent
 
-        const token = localStorage.getItem("authToken");
+      const token = localStorage.getItem("authToken");
         
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
-    const sessionToken = localStorage.getItem("sessionToken");
+    // const sessionToken = localStorage.getItem("sessionToken");
+    // if (sessionToken) {
+    //   config.params = {
+    //     ...config.params,
+    //     session_token: sessionToken,
+    //   };
+    // }
+  
+    const sessionToken = getSessionToken();
     if (sessionToken) {
       config.params = {
         ...config.params,
